a7e15c7d35958549511a8da28f47a49667ef8dad
Bobby Norton <bobby@testedminds.com>
2015-12-18 05:44:30 -0500

    Add pathfinding for undirected graphs

--- src/edgewise/analytics.clj
+++ src/edgewise/analytics.clj
@@ -20,3 +20,15 @@
      (select-vals (:edge-data (:g t)) (:edge t))))
   ([t & keys]
    (mapv #(select-vals % keys) (props t))))
+
+(defn- paths [g dest-id path visited]
+  (let [vertex (:inV (peek path))]
+    (if (= dest-id vertex) [(rest path)]
+        (let [out-e-props (-> (v g vertex) outE props)
+              valid-edge-props (filter #(not (contains? visited (:inV %))) out-e-props)]
+          (mapcat #(paths g dest-id (conj path %) (conj visited (:inV %))) valid-edge-props)))))
+
+(defn findpaths [g source-label dest-label]
+  (let [source-id (label-index g source-label)
+        dest-id   (label-index g dest-label)]
+    (paths g dest-id [{:inV source-id}] #{source-id})))
