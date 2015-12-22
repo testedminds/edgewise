8b26545834caf2ddc4f2c915e62e0a197bd6a3b4
Bobby Norton <bobby@testedminds.com>
2015-12-22 15:49:16 -0600

    Fix bug in rank

--- src/edgewise/pagerank.clj
+++ src/edgewise/pagerank.clj
@@ -1,32 +1,5 @@
 (in-ns 'edgewise.core)
 
-(defn- rank-increment [data v-id change]
-  (let [rank ((:rank data) v-id)]
-    (assoc-in data [:rank v-id] (+ rank change))))
-
-(defn- rank-neighbors [data v-id]
-  (let [vertex (v (:g data) v-id)
-        rank ((:rank data) v-id)
-        neighbors (-> vertex outE inV (props :_id) flatten)
-        num-neighbors (count neighbors)]
-    ;; if v has any outgoing edges, divide current rank equally among them.
-    (if (zero? num-neighbors) data
-        (let [change (/ rank num-neighbors)]
-          (reduce #(rank-increment %1 %2 change) data neighbors)))))
-
-(defn- new-rank [data v-id]
-  (let [rank ((:rank data) v-id)
-        c (:constant data)
-        d (:damping data)]
-    (assoc-in data [:rank v-id] (+ c (* d rank)))))
-
-(defn- increment-rank [data]
-  (let [vertices (:vertex (v (:g data)))
-        updates (reduce rank-neighbors data vertices)]
-    (reduce new-rank updates vertices)))
-
-;;;;;;;;;;;
-
 ;; map over all vertices to get a vector of a map of changes.
 ;;; get the ids of the outgoing edges and incoming vertices
 ;;; compute the change in rank for these neighbors contributed by the vertex.
@@ -41,10 +14,10 @@
         neighbor-ids (-> vertex outE inV (props :_id) flatten)
         num-neighbors (count neighbor-ids)]
     (if (zero? num-neighbors)
-      (let [allocation (/ rank (count (:vertex (v (:g data)))))]  ; if a node has no outE, divide current rank equally among all the nodes
+      (let [allocation (/ rank (:vertex-count data))] ; if a node has no outE, divide current rank equally among all the nodes
         {:all allocation})
       (let [allocation (/ rank num-neighbors)] ; if v has any outE, divide current rank equally among them.
-        (reduce #(assoc %1 %2 (+ ((:rank data) %2) allocation)) {} neighbor-ids)))))
+        (reduce #(assoc %1 %2 allocation) {} neighbor-ids)))))
 
 (defn- rank [new-rank-increments data v-id]
   (let [r (get new-rank-increments v-id 0)
@@ -55,7 +28,7 @@
 
 (defn- new-increment-rank [data]
   (let [all-v-ids (:vertex (v (:g data)))
-        updates (map #(rank-to-neighbors data %) all-v-ids)
+        updates (pmap #(rank-to-neighbors data %) all-v-ids)
         new-rank-increments (apply merge-with + updates)]
     ;; at this point, some of the v's have updated values
     ;; reduce all-v-ids to compute a new rank in data with a fn that looks up the new value from the increments (or 0), and then
@@ -67,8 +40,12 @@
 (defn diffusion [g damping-factor n]
   (let [vertex-ids (:vertex (v g))
         constant (/ (- 1 damping-factor) (count vertex-ids))
-        rank (reduce (fn [acc x] (assoc acc x 0.00)) {:all 0} vertex-ids)
-        data {:rank rank :constant constant :g g :damping damping-factor}]
+        rank (reduce (fn [acc x] (assoc acc x (/ 1.0 (count vertex-ids)))) {:all 0} vertex-ids)
+        data {:rank rank
+              :constant constant
+              :g g
+              :damping damping-factor
+              :vertex-count (count (:vertex (v g)))}]
     (sort-by val > (:rank (nth (iterate new-increment-rank data) n)))))
 
 ;; prints the rank with the label instead of the id
@@ -99,5 +76,11 @@
 ;; (-> (v g 1) outE inV inE outV (props :label))
 ;;(diffusion example 0.85 5)
 
+;; turn this reference example into a nice test.
 
+;; check that everything sums to 1: (reduce #(+ %1 (second %2)) 0 (butlast (diffusion example 0.85 50)))
+;; make this check part of the production code.
 
+;; put in a perf test to get the rumtime under 1s for 10000 iterations on this toy graph.
+
+;; add an option to run to convergence: The computation ends when for some small \epsilon |R(t+1) - R(t)|
