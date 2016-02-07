d8fa9d5e8d5e0bf585d50766e661a7d49fe80e28
Sarah Aslanifar <sarah@testedminds.com>
2016-02-06 23:33:07 -0600

    Remove cider-nrepl as a plugin dependency; defer to ~/.lein/profiles.clj

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :pedantic? :warn
-  :profiles {:dev {:plugins [[cider/cider-nrepl "0.10.1"]
-                             [lein-gorilla "0.3.5"]]}
+  :profiles {:dev {:plugins [[lein-gorilla "0.3.5"]]}
              :test {:dependencies [[gorilla-repl "0.3.5"]]}}
   :test-paths ["test" "doc"]
   :test-selectors {:default (fn [m] (not (or (:perf m) (:doc m))))
