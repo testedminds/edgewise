e5645c3ac02660ade55e096e1784051c85c4f096
Bobby Norton <bobby@testedminds.com>
2016-04-07 22:04:04 -0500

    Bump clojure and gorilla versions

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "edgewise.testedminds.com"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :pedantic? :warn
-  :profiles {:dev {:plugins [[lein-gorilla "0.3.5"]]}
-             :test {:dependencies [[gorilla-repl "0.3.5"]]}}
+  :profiles {:dev {:plugins [[lein-gorilla "0.3.6"]]}
+             :test {:dependencies [[gorilla-repl "0.3.6"]]}}
   :test-paths ["test" "doc"]
   :test-selectors {:default (fn [m] (not (or (:perf m) (:doc m))))
                    :perf :perf
